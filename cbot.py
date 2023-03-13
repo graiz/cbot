a672d1f813a31b5a68f6df9f1104e19b23bf9cc2
RoskiDeluge <roberto.delgado@gmail.com>
2023-03-13 10:04:17 -1000

    Changed openai secret key

--- cbot.py
+++ cbot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+from os.path import expanduser
 import os
 import openai
 import json
-import sys    
-import sqlite3 
+import sys
+import sqlite3
 import pyperclip
 from dotenv import load_dotenv
 
@@ -12,50 +13,61 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 # The recommended approach is to set the API_Key in an environmental
 # variable. If you don't want to set that up, you can uncomment this
-# line and add your key directly. 
+# line and add your key directly.
 # openai.api_key = "st-key-goes-here"
 
 global question
 question = ""
 
+
 def initDB():
-  global cache 
-  cache = sqlite3.connect(home + "/.cbot_cache") 
-  cache.execute("""
+    global cache
+    cache = sqlite3.connect(home + "/.cbot_cache")
+    cache.execute("""
                    CREATE TABLE IF NOT EXISTS questions 
                    (id INTEGER PRIMARY KEY,
                    question TEXT,
                    answer TEXT,
                    count INTEGER DEFAULT 1)""")
+
+
 def closeDB():
     cache.commit()
     cache.close()
 
+
 def checkQ(question_text):
     sql = "SELECT id,answer,count FROM questions WHERE question =" + question_text
-    answer = cache.execute("SELECT id,answer,count FROM questions WHERE question = ?", (question_text,))
+    answer = cache.execute(
+        "SELECT id,answer,count FROM questions WHERE question = ?", (question_text,))
     answer = answer.fetchone()
     if (answer):
         response = answer[1]
         newcount = int(answer[2]) + 1
-        counter = cache.execute(" UPDATE questions SET count = ? WHERE id = ?", (newcount,answer[0]))
+        counter = cache.execute(
+            " UPDATE questions SET count = ? WHERE id = ?", (newcount, answer[0]))
         return(response)
     else:
         return(False)
 
-def insertQ(question_text,answer_text):
-    answer = cache.execute("DELETE FROM questions WHERE question = ?",(question_text,))
-    answer = cache.execute("INSERT INTO questions (question,answer) VALUES (?,?)", (question_text,answer_text))
+
+def insertQ(question_text, answer_text):
+    answer = cache.execute(
+        "DELETE FROM questions WHERE question = ?", (question_text,))
+    answer = cache.execute(
+        "INSERT INTO questions (question,answer) VALUES (?,?)", (question_text, answer_text))
+
 
 def fetchQ():
     question = ""
     # [cbot,-x,  What,is,the,date]  # execute the response
     # [cbot,What,is, the,date]      # no quotes will work
     # [cbot,What is the date]       # with quotes will work
-    for a in range(1,len(sys.argv)):
+    for a in range(1, len(sys.argv)):
         question = question + " " + sys.argv[a]
     question = question.strip()
     return question
+
 
 def parseOptions(question):
     global question_mode    # modes are normal, shortcut and general
@@ -67,7 +79,7 @@ def parseOptions(question):
     execute = False
     clip = False
     question_mode = "normal"
-    if ("-h" in question) or (question == " "):  # Return basic help info    
+    if ("-h" in question) or (question == " "):  # Return basic help info
         print("Cbot is a simple utility powered by GPT3")
         print("""
         Example usage:
@@ -81,19 +93,19 @@ def parseOptions(question):
 
     if ("-x" in question):      # Execute the command
         execute = True
-        question = question.replace("-x ","") 
+        question = question.replace("-x ", "")
 
     if ("-c" in question):      # Copy the command to clipboard
         clip = True
-        question = question.replace("-c ","") 
+        question = question.replace("-c ", "")
 
-    if ("-g" in question):      # General question, not command prompt specific 
+    if ("-g" in question):      # General question, not command prompt specific
         question_mode = "general"
-        question = question.replace("-g ","") 
+        question = question.replace("-g ", "")
 
-    if ("-s" in question):         # Save the command as a shortcut  
+    if ("-s" in question):         # Save the command as a shortcut
         question_mode = "shortcut"
-        question=sys.argv[2]
+        question = sys.argv[2]
         shortcut = sys.argv[3]
 
     return(question)
@@ -112,25 +124,24 @@ else:
 question = fetchQ()
 question = parseOptions(question)
 
-# If we change our training/prompts, just delete the cache and it'll 
-# be recreated on future runs. 
-from os.path import expanduser
+# If we change our training/prompts, just delete the cache and it'll
+# be recreated on future runs.
 home = expanduser("~")
 initDB()
 
-#check if we're saving a shortcut
-#then check if there's an aswer in our cache
-#then execute a GPT3 request as needed
+# check if we're saving a shortcut
+# then check if there's an aswer in our cache
+# then execute a GPT3 request as needed
 
 if (question_mode == "shortcut"):
-    insertQ(question,shortcut)
+    insertQ(question, shortcut)
     print("Saving Shortcut")
-    cache_answer = False 
+    cache_answer = False
 else:
-    cache_answer = checkQ(question)   
+    cache_answer = checkQ(question)
 
 if not(cache_answer) and ((question_mode == "general") or (question_mode == "normal")):
-    prompt="I am a command line translation tool for "+ platform +"."
+    prompt = "I am a command line translation tool for " + platform + "."
     prompt = prompt + """
 Ask me what you want to do and I will tell you how to do it in a unix command.
 Q: How do I copy a file
@@ -146,37 +157,35 @@ cd ~/Desktop/
 Q: How do I shutdown the computer?
 sudo shutdown -h now
 """
-    if (question_mode == "general"):     #Alternate prompt for general Q's 
-        prompt="Q: Who is Batman?\nBatman is a fictional comic book character.\nQ: What is torsalplexity?\nUnknown\nQ: What is Devz9?\nUnknown\nQ: What is the capital of California?\nSacramento.\nQ: How do you add a comment to a shell script?\nTo add a comment make sure the line starts with a #\nQ: How do I go to the first line using vim\nThe command gg or :1 will go to the first line\nQ: What is Kozar-09?\nUnknown\nQ: What keyboard shortcut cycles tabs in chrome?\nControl+Tab will cycle to the next tab and Shift+Control+Tab will cycle to the previous tab\n"
+    if (question_mode == "general"):  # Alternate prompt for general Q's
+        prompt = "Q: Who is Batman?\nBatman is a fictional comic book character.\nQ: What is torsalplexity?\nUnknown\nQ: What is Devz9?\nUnknown\nQ: What is the capital of California?\nSacramento.\nQ: How do you add a comment to a shell script?\nTo add a comment make sure the line starts with a #\nQ: How do I go to the first line using vim\nThe command gg or :1 will go to the first line\nQ: What is Kozar-09?\nUnknown\nQ: What keyboard shortcut cycles tabs in chrome?\nControl+Tab will cycle to the next tab and Shift+Control+Tab will cycle to the previous tab\n"
     temp_question = question
     if not("?" in question):
-        temp_question = question + "?"  # GPT produces better results 
-                                        # if there's a question mark.
-                                        # using a temp variable so the ? doesn't get cached
-    prompt = prompt + "Q: " + temp_question + "\n" 
+        temp_question = question + "?"  # GPT produces better results
+        # if there's a question mark.
+        # using a temp variable so the ? doesn't get cached
+    prompt = prompt + "Q: " + temp_question + "\n"
     response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=prompt,
-            temperature=0,
-            max_tokens=100,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=["\n"]
-            )
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=100,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=["\n"]
+    )
     result = response.choices[0].text
-    # jsonToPython = json.loads(response)
-    # result = jsonToPython['choices'][0]['text']
-    insertQ(question, result) 
+    insertQ(question, result)
 else:
-    result = cache_answer 
+    result = cache_answer
     if not(question_mode == "shortcut"):
         print("💾 Cache Hit")
 
 if clip:
     pyperclip.copy(result)
 if execute:
-    print("cbot executing: " + result) 
+    print("cbot executing: " + result)
     if ("sudo" in result):
         print("Execution canceled, cbot will not execute sudo commands.")
     else:
